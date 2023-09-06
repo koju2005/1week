191b3d7984ae0775bf177c453273b417e465c2fe
koju2005 <141552941+koju2005@users.noreply.github.com>
2023-09-06 12:11:25 +0900

    버튼효과음

--- Assets/Scripts/MenuScene/Login.cs
+++ Assets/Scripts/MenuScene/Login.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Login : MonoBehaviour
 {
+    SoundManager soundManager;
+    AudioSource m_AudioSource;
     public TMP_InputField inputname;
-   public void OnClick()
+    public void Awake()
     {
+   
+    }
+    public void Start()
+    {
+
+    }
+    public void OnClick()
+    {
+        SoundManager.instance.PlaySound();
         PlayerPrefs.SetString("Name", inputname.text);
-        SceneManager.LoadScene("MainScene");
+       SceneManager.LoadScene("MainScene");
     }
 }
